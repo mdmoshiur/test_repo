3b9bb6c78692221f7d7639e2905b5ffb9594da1d
Md Moshiur Rahman <moshiur1176@gmail.com>
2021-06-26 14:37:24 +0600

    changes have made

--- example_new.py
+++ example_new.py
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-def git_opeation():
- print("I am adding example.py file to the remote repository.")
-git_opeation()
-=======
+hello world
 
->>>>>>> 48b3282c9baccb3d3773d40577d33f6eb38ea244
